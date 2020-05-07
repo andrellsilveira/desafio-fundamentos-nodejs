e1092d52205108f25add67815a69b42b85ba6bd5
André Silveira <andrellsilveira@gmail.com>
2020-05-07 19:43:30 -0300

    Desafio completado com sucesso

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
+interface ObjectTransactionAndBalance {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +25,53 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ObjectTransactionAndBalance {
+    const balance = this.getBalance();
+
+    const transactionsAndBalance: ObjectTransactionAndBalance = {
+      transactions: this.transactions,
+      balance,
+    };
+
+    return transactionsAndBalance;
+  }
+
+  public findByTitleAndType(title: string, type: string): Transaction | null {
+    const transactionSameTitleAndType = this.transactions.find(
+      transaction => transaction.title === title && transaction.type === type,
+    );
+
+    return transactionSameTitleAndType || null;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let sunIncome = 0;
+    let sunOutcome = 0;
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        sunIncome += transaction.value;
+      }
+      if (transaction.type === 'outcome') {
+        sunOutcome += transaction.value;
+      }
+    });
+
+    const total = sunIncome - sunOutcome;
+
+    const balance: Balance = {
+      income: sunIncome,
+      outcome: sunOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(data: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction(data);
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(data: RequestDTO): Transaction {
+    const transactionSameTitleAndType = this.transactionsRepository.findByTitleAndType(
+      data.title,
+      data.type,
+    );
+
+    if (transactionSameTitleAndType) {
+      throw Error('Já existe uma transação com o título e tipo informados.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title: data.title,
+      type: data.type,
+      value: data.value,
+    });
+
+    return transaction;
   }
 }
 
